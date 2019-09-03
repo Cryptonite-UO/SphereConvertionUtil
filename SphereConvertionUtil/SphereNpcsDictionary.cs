ba1ceeb8f8ae1e0092df14ae92088a05e5419093
Jhobean <51728381+Jhobean@users.noreply.github.com>
2019-09-02 21:23:44 -0400

    Update SphereNpcsDictionary.cs

--- SphereConvertionUtil/SphereNpcsDictionary.cs
+++ SphereConvertionUtil/SphereNpcsDictionary.cs
@@ -90,17 +90,17 @@ namespace SphereConvertionUtil
 						
             Npcs.Add("c_dracoliche", "c_skeletal_dragon");
 						
-            Npcs.Add("c_h_bandit_m_crossbow", "c_bandit_m"); // Faudrait en trouver 1 avec un arbalete pour faire pareil ( buccanner's den)
+            Npcs.Add("c_h_bandit_m_crossbow", "c_bandit_m_crossbow");
 						
-            Npcs.Add("c_h_bandit_f_crossbow", "c_bandit_f"); // Faudrait en trouver 1 avec un arbalete pour faire pareil ( buccanner's den)
+            Npcs.Add("c_h_bandit_f_crossbow", "c_bandit_f_crossbow");
 						
-            Npcs.Add("c_bandit_beserk1", "c_bandit_m"); // ( buccanner's den)
+            Npcs.Add("c_bandit_beserk1", "c_brigand");
 								
-            Npcs.Add("c_bandit_beserk2", "c_bandit_m"); // ( buccanner's den)
+            Npcs.Add("c_bandit_beserk2", "c_brigand");
 								
-            Npcs.Add("c_bandit_beserk3", "c_bandit_m"); // ( buccanner's den)
+            Npcs.Add("c_bandit_beserk3", "c_brigand_f");
 								
-            Npcs.Add("c_bandit_beserk4", "c_bandit_m"); // ( buccanner's den)
+            Npcs.Add("c_bandit_beserk4", "c_brigand_f");
 		
             Npcs.Add("c_troll_w_mace", "c_troll_w_axe");
 
