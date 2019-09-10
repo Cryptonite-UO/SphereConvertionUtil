82371a8c2e7055770558ef209d60ee5b4164ec1a
Jhobean <51728381+Jhobean@users.noreply.github.com>
2019-09-09 21:44:36 -0400

    Update SphereItemsDictionary.cs

--- SphereConvertionUtil/SphereItemsDictionary.cs
+++ SphereConvertionUtil/SphereItemsDictionary.cs
@@ -24,8 +24,6 @@ namespace SphereConvertionUtil
 
             Items.Add("i_BRUSH_HORSE_4", "i_brush_horse_2");
 
-            Items.Add("i_deed_ship_dragon_long_N", "i_deed_large_dragon_ship_N");
-
             Items.Add("i_mt_ostard_zostrich", "i_mt_ostard_frenzied");
 
             Items.Add("i_deed_ship_small_N", "i_deed_small_ship_n");
@@ -36,7 +34,7 @@ namespace SphereConvertionUtil
 
             Items.Add("i_demi_elfe_stone", "i_stone_race_demielfe");
 
-            Items.Add("i_decorative_armor_2", "0151c"); //Pas sur que mettre un ID ca fonctionne
+            Items.Add("i_decorative_armor_2", "i_decorative_armor");
 
             Items.Add("i_bookcase_peter", "i_bookcase_ruined");
 
@@ -45,6 +43,66 @@ namespace SphereConvertionUtil
             Items.Add("i_chair_wood_bamboo", "i_chair_wood_2");
                                     
             Items.Add("i_death_robe", "i_deathshroud_craft"); // ROBE que je crois inutile mais au cas que qqn en avait une
+                                                
+            Items.Add("i_bottles_wine", "i_bottle_wine");
+                                                
+            Items.Add("i_CARDS_PLAYING", "i_cards");
+                                                            
+            Items.Add("i_elven_male_shirt", "i_elven_shirt");
+                                                
+            Items.Add("i_elven_male_shirt2", "i_elven_shirt");
+                                                            
+            Items.Add("i_deed_ship_medium_N", "i_deed_medium_ship_n");
+                                                
+            Items.Add("i_deed_ship_dragon_small_N", "i_deed_small_dragon_ship_n");
+                                                            
+            Items.Add("i_deed_ship_dragon_medium_N", "i_deed_medium_dragon_ship_n");
+            
+            Items.Add("i_deed_ship_dragon_long_N", "i_deed_large_dragon_ship_N");
+                                                            
+            Items.Add("i_deed_ship_long_N", "i_deed_large_ship_n");
+                                                            
+            Items.Add("i_bench_wood_smooth_end", "i_bench_wood_smooth");
+                                                            
+            Items.Add("i_bench_wood_smooth_ext", "i_bench_wood_smooth");//Flip pour différente partie
+                                                            
+            Items.Add("i_table_wood_smooth_narrow_ext", "i_table_wood_smooth_narrow_end");//Flip pour différente partie
+                                                            
+            Items.Add("i_table_wood_solid_custom_corner_ns", "i_table_wood_solid_narrow_end");//Flip pour différente partie
+                                                            
+            Items.Add("i_table_wood_solid_custom_corner_ew", "i_table_wood_solid_narrow_end");//Flip pour différente partie
+                                                                        
+            Items.Add("i_pet_daemon", "i_pet_demon");
+                                                                                                                                                            
+            Items.Add("i_pegbaord", "i_pegboard");
+                                                                                                
+            Items.Add("i_table_runner_snowflake", "i_table_runner_red");
+                                                                                                
+            Items.Add("i_table_runner_plaid_blue", "i_table_runner_blue");
+                                                                                                            
+            Items.Add("i_fish_cooked", "i_fish_cooked_headless");
+                                                                                                                                                                                                            
+            Items.Add("i_bed_21", "0a7a");//Flip pour différente partie
+                                                                                                
+            Items.Add("i_bed_20", "0a7a");//Flip pour différente partie
+                                                                                                                        
+            Items.Add("i_bed_2", "i_bed_1");//Flip pour différente partie
+                                                                                                
+            Items.Add("i_bed_5", "i_bed_6");//Flip pour différente partie
+                                                                                                                        
+            Items.Add("i_bed_25", "0a80");//Flip pour différente partie
+                                                                                                
+            Items.Add("i_bed_24", "0a80");//Flip pour différente partie
+                                                                                                                                    
+            Items.Add("i_bed_26", "0a80");//Flip pour différente partie
+                                                                                                
+            Items.Add("i_bed_27", "0a80");//Flip pour différente partie
+                                                                                                                        
+            Items.Add("i_tapestry_5_w", "i_tapestry_5_n");//Flip pour différente partie
+                                                                                                                                    
+            Items.Add("i_pet_ostard_zostrich", "i_pet_ostard_frenzied");
+              
+                          
 
             return Items;
         }
