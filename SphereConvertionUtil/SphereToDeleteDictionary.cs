cc8703b6edd1ddb7164580120d744d25e7879d99
Nirad <miljours@gmail.com>
2019-09-29 16:51:13 -0400

    enlever double de la liste

--- SphereConvertionUtil/SphereToDeleteDictionary.cs
+++ SphereConvertionUtil/SphereToDeleteDictionary.cs
@@ -8,11 +8,7 @@ namespace SphereConvertionUtil
         private static List<String> DeleteList = new List<string>();
 
         public static List<string> Init()
-        {
-            DeleteList.Add("i_arena_random");
-
-            DeleteList.Add("i_arena_random_2v2");
-            
+        {            
             DeleteList.Add("i_pierre_depecage_dragon_small_red");
                         
             DeleteList.Add("i_pierre_depecage_dragon_small_black");
