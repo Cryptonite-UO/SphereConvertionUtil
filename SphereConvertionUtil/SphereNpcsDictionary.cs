213d7bbfd7e7cf853b61370603b13b599fbb6af5
FiLoUs64 <perras.philippe@outlook.com>
2019-10-06 11:39:21 -0400

    Modif pour Treant

--- SphereConvertionUtil/SphereNpcsDictionary.cs
+++ SphereConvertionUtil/SphereNpcsDictionary.cs
@@ -48,7 +48,7 @@ namespace SphereConvertionUtil
 
             Npcs.Add("c_ophidian_mage", "c_ophidian_archmage");
 
-            Npcs.Add("c_m_treant", "c_reaper_redux");//Remplacé par un nouveau
+            //Npcs.Add("c_m_treant", "c_reaper_redux");//Remplacé par un nouveau
 
             Npcs.Add("c_m_icefiend", "c_demon_ice");
 
